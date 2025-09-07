ff55be32f9e10774b98ca385620644e5ef35efed
IKK999 <147667893+IKK999@users.noreply.github.com>
2025-09-06 21:29:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function GoogleSearch() {
+    var userInput = document.getElementById("userInput").value;
+    window.location.href = "https://www.google.com/search?q=" + userInput + "&sca_esv=26a7554b929691b3&sxsrf=AHTn8zoEBcH-suz2nEhcq0AE51fXh6lU5g%3A1737982855719&source=hp&ei=h4OXZ-XQKbOH0PEP9enBwAc&iflsig=ACkRmUkAAAAAZ5eRl1UyKph3nllzgWf4qrbLOanF317f&ved=0ahUKEwilsPTV-pWLAxWzAzQIHfV0EHgQ4dUDCBA&uact=5&oq=hi&gs_lp=Egdnd3Mtd2l6IgJoaTIKECMYgAQYJxiKBTIEECMYJzIKECMYgAQYJxiKBTIKEC4YgAQYQxiKBTIKEAAYgAQYQxiKBTIKEAAYgAQYQxiKBTIKEC4YgAQYQxiKBTIKEAAYgAQYQxiKBTIKEAAYgAQYQxiKBTIOEAAYgAQYsQMYgwEYigVIowZQwAJYygNwAXgAkAEAmAGNAaABgQKqAQMwLjK4AQPIAQD4AQGYAgOgAosCqAIKwgIHECMYJxjqAsICChAjGPAFGCcY6gKYAwTxBVcFpmoK6loHkgcDMS4yoAf1Fg&sclient=gws-wiz";
+}
+
+function FeelingLucky() {
+    var userInput = document.getElementById("userInput").value;
+    window.location.href = "https://www.google.com/search?q=" + userInput + "&btnI=I";
+}
+
+function ImageSearch() {
+    var userInput = document.getElementById("userInput").value;
+    window.location.href = "https://www.google.com/search?q=" + userInput + "&sca_esv=26a7554b929691b3&udm=2&biw=1214&bih=638&sxsrf=AHTn8zrrN2qiRuhFtZBxpRedTnnZSwOIyA%3A1737983626836&ei=ioaXZ5XdMoiz0PEPgLmFmAE&ved=0ahUKEwiVys_F_ZWLAxWIGTQIHYBcARMQ4dUDCBE&uact=5&oq=w&gs_lp=EgNpbWciAXcyChAAGIAEGEMYigUyCBAAGIAEGLEDMgsQABiABBixAxiDATILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMggQABiABBixAzIOEAAYgAQYsQMYgwEYigUyBRAAGIAEMgUQABiABDIOEAAYgAQYsQMYgwEYigVI6xBQuwhY2Q5wAngAkAEAmAFWoAFWqgEBMbgBA8gBAPgBAZgCA6ACX6gCB8ICBxAjGCcY6gKYAwKIBgGSBwEzoAfeBA&sclient=img";
+}
+
+function AdvancedSearch() {
+    var userInput1 = document.getElementById("input1").value;
+    var userInput2 = document.getElementById("input2").value;
+    var userInput3 = document.getElementById("input3").value;
+    var userInput4 = document.getElementById("input4").value;
+    window.location.href = "https://www.google.com/search?hl=en&as_q=" + userInput1 + "&as_epq=" + userInput2 + "&as_oq=" + userInput3 + "&as_eq=" + userInput4 + "&as_nlo=&as_nhi=&lr=&cr=&as_qdr=all&as_sitesearch=&as_occt=any&as_filetype=&tbs=";
+}
